79b12322c224f7ddd40bb872911ed862ca1bad4f
h2oaquarian <81542435+hayashikobo@users.noreply.github.com>
2021-06-19 22:38:31 +0800

    update 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,8 @@
 	var $window = $(window),
 		$body = $('body'),
 		$header = $('#header'),
-		$titleBar = null,
-		$nav = $('#nav'),
+//		$titleBar = null,
+//		$nav = $('#nav'),
 		$wrapper = $('#wrapper');
 
 	// Breakpoints.
@@ -54,106 +54,178 @@
 
 			}
 
+	
+  /**
+   * Easy selector helper function
+   */
+   const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
+	
+  /**
+   * Back to top button
+   */
+   let backtotop = select('.back-to-top')
+   if (backtotop) {
+	 const toggleBacktotop = () => {
+	   if (window.scrollY > 100) {
+		 backtotop.classList.add('active')
+	   } else {
+		 backtotop.classList.remove('active')
+	   }
+	 }
+	 window.addEventListener('load', toggleBacktotop)
+	 onscroll(document, toggleBacktotop)
+   }
+ 
+   /**
+	* Mobile nav toggle
+	*/
+   on('click', '.mobile-nav-toggle', function(e) {
+	 select('body').classList.toggle('mobile-nav-active')
+	 this.classList.toggle('bx-menu')
+	 this.classList.toggle('bx-arrow-back')
+   })
+	
+/**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+ on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+
+  /**
+   * Hero type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+	
+	
 	// Header Panel.
 
-		// Nav.
-			var $nav_a = $nav.find('a');
+	// 	// Title Bar.
+	// 		$titleBar = $(
+	// 			'<div id="titleBar">' +
+	// 				'<a href="#header" class="toggle"></a>' +
+	// 				'<span class="title">' + $('#logo').html() + '</span>' +
+	// 			'</div>'
+	// 		)
+	// 			.appendTo($body);
 
-			$nav_a
-				.addClass('scrolly')
-				.on('click', function() {
+	// 	// Panel.
+	// 		$header
+	// 			.panel({
+	// 				delay: 500,
+	// 				hideOnClick: true,
+	// 				hideOnSwipe: true,
+	// 				resetScroll: true,
+	// 				resetForms: true,
+	// 				side: 'right',
+	// 				target: $body,
+	// 				visibleClass: 'header-visible'
+	// 			});
 
-					var $this = $(this);
+	// // Scrolly.
+	// 	$('.scrolly').scrolly({
+	// 		speed: 1000,
+	// 		offset: function() {
 
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
+	// 			if (breakpoints.active('<=medium'))
+	// 				return $titleBar.height();
 
-					// Deactivate all links.
-						$nav_a.removeClass('active');
+	// 			return 0;
 
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
-
-				})
-				.each(function() {
-
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
-
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
-
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '5vh',
-							bottom: '5vh',
-							initialize: function() {
-
-								// Deactivate section.
-									$section.addClass('inactive');
-
-							},
-							enter: function() {
-
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($nav_a.filter('.active-locked').length == 0) {
-
-										$nav_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		// Title Bar.
-			$titleBar = $(
-				'<div id="titleBar">' +
-					'<a href="#header" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$header
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'header-visible'
-				});
-
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
-
-				if (breakpoints.active('<=medium'))
-					return $titleBar.height();
-
-				return 0;
-
-			}
-		});
+	// 		}
+	// 	});
 
 })(jQuery);
